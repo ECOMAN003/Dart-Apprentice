b7f0ba73522f8d9b0aaf0f6718935533f4597994
ECOMAN003 <ekiliheman@gmail.com>
2022-08-24 07:46:03 +0100

    some revision

--- Section 1/Chapter 2/chapter_two/bin/chapter_two.dart
+++ Section 1/Chapter 2/chapter_two/bin/chapter_two.dart
@@ -1,3 +1,20 @@
-void main(List<String> arguments) {
-  print('Hello world: Dart Apprentice Reader');
+enum Weather { windy, cloudy, rainy, sunny }
+
+void main() {
+  const weatherToday = Weather.cloudy;
+  switch (weatherToday) {
+    case Weather.rainy:
+      print('Take An Umbrealla');
+      break;
+    case Weather.sunny:
+      print('Take some Shades');
+      break;
+    case Weather.cloudy:
+      print('It could Rain today');
+      break;
+    default:
+      print('Cloudy With a ChanCE OF MEATBALLS');
+  }
+
+
 }
