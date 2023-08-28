d2399ab40145b3de4dca079636da8d83d9375623
ECOMAN003 <ekiliheman@gmail.com>
2023-08-28 10:58:21 +0100

    finally solved it

--- Section 1/Chapter 7/chapter_7_2.dart
+++ Section 1/Chapter 7/chapter_7_2.dart
@@ -6,11 +6,12 @@ import 'dart:math';
 
 void main(List<String> args) {
   int? result = randomly();
+  result ??= 0;
   print(result);
 }
 
 int? randomly() {
-  if (Random().nextInt(1) == 0) {
+  if (Random().nextInt(2) == 0) {
     return null;
   }
   return 42;
