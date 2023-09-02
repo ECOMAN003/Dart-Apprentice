f2054ba2f976ac205a8c45da5ad39d012987d276
ECOMAN003 <ekiliheman@gmail.com>
2023-09-02 10:55:20 +0100

    finished secnod exercise

--- Section 1/Chapter 7/chapter_7_2.dart
+++ Section 1/Chapter 7/chapter_7_2.dart
@@ -5,9 +5,13 @@
 import 'dart:math';
 
 void main(List<String> args) {
-  int? result = randomly();
-  result ??= 0;
-  print(result);
+  // int? result = randomly();
+  // result ??= 0;
+  // print(result);
+
+  Name myName = Name(true);
+  myName.surname = "Heman";
+  print(myName.toString());
 }
 
 int? randomly() {
@@ -19,7 +23,23 @@ int? randomly() {
 
 //exercise 2
 class Name {
+  Name(this.surnsmeIsFirst) {
+    surname ??= "";
+    givenName ??= "";
+  }
+
   String? surname;
   String? givenName;
-  late final String? fullName;
+  late final String? _fullName;
+  final bool surnsmeIsFirst;
+
+  @override
+  String toString() {
+    if (surnsmeIsFirst == true) {
+      _fullName = surname! + " " + givenName!;
+    } else {
+      _fullName = givenName! + " " + surname!;
+    }
+    return _fullName!;
+  }
 }
