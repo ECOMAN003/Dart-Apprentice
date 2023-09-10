556f5ae9e5f4224d8065e4287fcb987a3d2e2a9f
ECOMAN003 <ekiliheman@gmail.com>
2023-09-10 18:15:46 +0100

    learned spread operator

--- Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
+++ Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
@@ -1,4 +1,7 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  print(desserts);
+}
 
 const pastries = ['cookies', 'croissants'];
 const candy = ['Junior Mints', 'Twizzlers', 'M&Ms'];
+const desserts = ['donuts', ...pastries, ...candy];
