7ae4267910da9056f5f0e685e3fec67efa0a980b
ECOMAN003 <ekiliheman@gmail.com>
2023-09-11 09:34:54 +0100

    collection if & collection for

--- Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
+++ Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
@@ -1,7 +1,34 @@
 void main(List<String> arguments) {
-  print(desserts);
+  //print(desserts);
+  //print(candy);
+  print(bigDeserts);
 }
 
 const pastries = ['cookies', 'croissants'];
-const candy = ['Junior Mints', 'Twizzlers', 'M&Ms'];
-const desserts = ['donuts', ...pastries, ...candy];
+//const candy = ['Junior Mints', 'Twizzlers', 'M&Ms'];
+//const desserts = ['donuts', ...pastries, ...candy];
+
+//null spread operator ?...
+List<String>? coffees;
+final hotDrinks = ['milk tea', ...?coffees];
+
+//collection if
+const peanutAllergy = false;
+
+const candy = [
+  'Junior Mints',
+  'Twizzlers',
+  if (peanutAllergy) 'Reeses',
+];
+
+//collection for
+const deserts = [
+  'gobi',
+  'sahara',
+  'arctic'
+];
+
+var bigDeserts = [
+  'ARABIAN',
+  for(var desert in deserts) desert.toUpperCase(),
+];
