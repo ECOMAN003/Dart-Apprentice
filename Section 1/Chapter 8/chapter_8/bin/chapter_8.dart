d1f80f54989a00fa2521d52accf33bc109e43359
ECOMAN003 <ekiliheman@gmail.com>
2023-09-12 13:58:57 +0100

    I thought I was done with Maps

--- Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
+++ Section 1/Chapter 8/chapter_8/bin/chapter_8.dart
@@ -1,28 +1,30 @@
 void main(List<String> arguments) {
   //print(desserts);
   //print(candy);
-  print(bigDeserts);
+  //print(bigDeserts);
 
 //exercises
 //#1
   List<String> months = [];
-  months.add('January');
-  months.add('February');
-  months.add('March');
-  months.add('April');
-  months.add('May');
-  months.add('June');
-  months.add('July');
-  months.add('August');
-  months.add('September');
-  months.add('October');
-  months.add('November');
-  months.add('December');
+  months.addAll([
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]);
 
   final List<String> immutableMonths = List.unmodifiable(months);
-  var upperCaseMonths = [for(var month in months) month.toUpperCase()];
+  var upperCaseMonths = [for (var month in months) month.toUpperCase()];
 
-  print(upperCaseMonths);
+  mapsExercises();
 }
 
 const pastries = ['cookies', 'croissants'];
@@ -49,3 +51,50 @@ var bigDeserts = [
   'ARABIAN',
   for (var desert in deserts) desert.toUpperCase(),
 ];
+
+//dart sets
+void sets() {
+  final Set<int> someSet = {1, 2, 3, 1};
+  final setA = {8, 2, 3, 1, 4};
+  final setB = {1, 6, 5, 4};
+
+  final intersection = setA.intersection(setB);
+//collectinos and most list operations work on sets
+}
+
+//Dart Maps
+void maps() {
+  final Map<String, int> inventory = {
+    'cakes': 20,
+    'pies': 14,
+    'donuts': 37,
+    'cookies': 141,
+    'brownies': 3
+  };
+
+//looping in Maps
+  for (var item in inventory.keys) {
+    print(inventory[item]);
+  }
+
+  for (final entry in inventory.entries) {
+    print('${entry.key} -> ${entry.value}');
+  }
+}
+
+//Maps Exercises
+void mapsExercises() {
+  Map<String, String> data = {
+    'name': 'Heman',
+    'profession': 'Student',
+    'country': 'Cameroon',
+    'city': 'Buea'
+  };
+
+  data['country'] = 'Canada';
+  data['city'] = 'Toronto';
+
+  for (var entry in data.entries) {
+    print('${entry.key} -> ${entry.value}');
+  }
+}
