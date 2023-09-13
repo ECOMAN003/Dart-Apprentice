2f7b4e7c39cfed1bbc6addbee265c330d8003227
ECOMAN003 <ekiliheman@gmail.com>
2023-09-13 19:30:30 +0100

    New Chapter: ahead aheadddd

---
+++ Section 1/Chapter 9/chapter_9/bin/chapter_9.dart
@@ -0,0 +1,5 @@
+import 'package:chapter_9/chapter_9.dart' as chapter_9;
+
+void main(List<String> arguments) {
+  print('Hello world: ${chapter_9.calculate()}!');
+}
---
+++ Section 1/Chapter 9/chapter_9/test/chapter_9_test.dart
@@ -0,0 +1,8 @@
+import 'package:chapter_9/chapter_9.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
