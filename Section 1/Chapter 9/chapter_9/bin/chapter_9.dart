13ceb63a0a8d96e998380340f5301f83bd40a5e6
ECOMAN003 <ekiliheman@gmail.com>
2023-09-17 22:25:47 +0100

    this thing is not easy bruh

--- Section 1/Chapter 9/chapter_9/bin/chapter_9.dart
+++ Section 1/Chapter 9/chapter_9/bin/chapter_9.dart
@@ -1,3 +1,25 @@
-void main(List<String> arguments) {
+void main(List<String> arguments) {}
 
+//subclasses
+enum Grade { A, B, C, D, F }
+
+class Person {
+  Person(this.givenName, this.surname);
+  String givenName;
+  String surname;
+  String get fullName => '$givenName $surname';
+
+  @override
+  String toString() => fullName;
+}
+
+class Student {
+  Student(this.givenName, this.surname);
+  String givenName;
+  String surname;
+  var grades = <Grade>[];
+  String get fullName => '$givenName $surname';
+
+  @override
+  String toString() => fullName;
 }
