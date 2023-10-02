bec2c40b9b576f2152e491ab3f599ff8795a8606
ECOMAN003 <ekiliheman@gmail.com>
2023-10-02 19:13:40 +0100

    Restarted, tired

--- Section 1/Chapter 9/chapter_9/bin/chapter_9.dart
+++ Section 1/Chapter 9/chapter_9/bin/chapter_9.dart
@@ -1,4 +1,21 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  useClass1();
+}
+
+//using the classes
+void useClass1() {
+  final jon = Person('Jon', 'Snow');
+  final jane = Student('Jane', 'Snow');
+  print(jon.fullName);
+  print(jane.fullName);
+}
+
+void useClass2() {
+  final jessie = SchoolBandMember('Jessie', 'Jones');
+  final marty = StudentAthlete('Marty', 'McFly');
+  final students = [jessie, marty];
+  print(int is Object);
+}
 
 //subclasses
 enum Grade { A, B, C, D, F }
@@ -13,13 +30,59 @@ class Person {
   String toString() => fullName;
 }
 
-class Student {
-  Student(this.givenName, this.surname);
-  String givenName;
-  String surname;
+class Student extends Person {
+  Student(String givenName, String surname) : super(givenName, surname);
   var grades = <Grade>[];
-  String get fullName => '$givenName $surname';
 
   @override
-  String toString() => fullName;
+  String get fullName => '$surname, $givenName';
 }
+
+class SchoolBandMember extends Student {
+  SchoolBandMember(String givenName, String surname)
+      : super(givenName, surname);
+  static const minimumPracticeTime = 2;
+}
+
+class StudentAthlete extends Student {
+  StudentAthlete(String givenName, String surname) : super(givenName, surname);
+  bool get isEligible => grades.every((grade) => grade != Grade.F);
+}
+
+//using initializer lists, super gets called last
+//class SomeChild extends SomeParent{
+//someChild(double height)
+//: assert(height != 0),
+//_height = height,
+//super();
+// 
+//final double _height;
+//}
+
+//mini-exercises
+class Fruit {
+  Fruit(this.color);
+  String color;
+  String describeColor() {
+    return 'This fruit is color $color';
+  }
+}
+
+class Melon extends Fruit {
+  Melon(String color) : super(color);
+}
+
+class Watermelon extends Fruit {
+  Watermelon(String color) : super(color);
+  @override
+  String describeColor() {
+    return 'color is $color';
+  }
+}
+
+class Cantaloupe extends Fruit {
+  Cantaloupe(String color) : super(color);
+}
+
+
+void miniExercise() {}
