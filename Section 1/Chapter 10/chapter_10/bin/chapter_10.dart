58a4412d0cb7413ccf0d17c48497b2b1029d086e
ECOMAN003 <ekiliheman@gmail.com>
2023-10-04 16:20:38 +0100

    will continue at 7

--- Section 1/Chapter 10/chapter_10/bin/chapter_10.dart
+++ Section 1/Chapter 10/chapter_10/bin/chapter_10.dart
@@ -1,3 +1,38 @@
-void main(List<String> arguments) {
-  
+Future<void> main() async {
+  print('Before the future');
+  final value = await Future<int>.delayed(
+    Duration(seconds: 1),
+    () => 42,
+  );
+  print('Value: $value');
+  print('After the future');
+  //testFuture();
+
 }
+
+//Examples of a future
+final myFuture = Future<int>.delayed(
+  Duration(seconds: 1),
+  () => 42,
+);
+
+///function to test futures
+void testFuture() {
+  print('Before the future');
+  final myFuture = Future<int>.delayed(
+    Duration(seconds: 1),
+    () => 42,
+  )
+      .then(
+        (value) => print('Value: $value'),
+      )
+      .catchError(
+        (error) => print('Error: $error'),
+      )
+      .whenComplete(
+        () => print('Future is complete'),
+      );
+  print('After the future');
+}
+
+//try-catch blocks with async-await
