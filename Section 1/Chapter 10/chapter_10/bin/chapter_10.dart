e87d93c12cd5d4547212d96c74f52d83e7c00a1d
ECOMAN003 <ekiliheman@gmail.com>
2023-10-06 21:56:23 +0100

    some http request stuff

--- Section 1/Chapter 10/chapter_10/bin/chapter_10.dart
+++ Section 1/Chapter 10/chapter_10/bin/chapter_10.dart
@@ -1,13 +1,23 @@
-Future<void> main() async {
-  print('Before the future');
-  final value = await Future<int>.delayed(
-    Duration(seconds: 1),
-    () => 42,
-  );
-  print('Value: $value');
-  print('After the future');
-  //testFuture();
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 
+Future<void> main() async {
+  final url = 'https://jsonplaceholder.typicode.com/todos/1';
+  final parsedUrl = Uri.parse(url);
+  final response = await http.get(parsedUrl);
+  final statusCode = response.statusCode;
+  if (statusCode == 200) {
+    final rawJsonString = response.body;
+    final jsonMap = jsonDecode(rawJsonString);
+    final todo = Todo.fromJson(jsonMap);
+    print(todo);
+  } else {
+    throw HttpException('$statusCode');
+  }
+
+  //testFuture();
+  //tryCatchBlocks();
 }
 
 //Examples of a future
@@ -36,3 +46,52 @@ void testFuture() {
 }
 
 //try-catch blocks with async-await
+Future<void> tryCatchBlocks() async {
+  print('Before the future');
+  try {
+    final value = await Future<int>.delayed(
+      Duration(seconds: 1),
+      () => 42,
+    );
+    throw Exception('There was an error');
+    print('Value: $value');
+  } catch (error) {
+    print(error);
+  } finally {
+    print('Future is complete');
+  }
+
+  print('After the future');
+}
+
+//asynchronous network requests
+class Todo {
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(Map<String, Object?> jsonMap) {
+    return Todo(
+      userId: jsonMap['userId'] as int,
+      id: jsonMap['id'] as int,
+      title: jsonMap['title'] as String,
+      completed: jsonMap['completed'] as bool,
+    );
+  }
+
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  @override
+  String toString() {
+    return 'userId: $userId\n'
+        'id: $id\n'
+        'title: $title\n'
+        'completed: $completed';
+  }
+}
